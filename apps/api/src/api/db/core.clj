daa59eb79b029907dda4a245b87d9d774b9e417d
Gabriel Specian <ga.specian@gmail.com>
2026-05-27 21:46:21 -0300

    fix: make connect! idempotent, add disconnect!, cache config

--- apps/api/src/api/db/core.clj
+++ apps/api/src/api/db/core.clj
@@ -6,10 +6,20 @@
 (defonce ^:private connection (atom nil))
 (defonce ^:private database   (atom nil))
 
+(def ^:private config
+  (delay (read-config (io/resource "config.edn"))))
+
 (defn connect! []
-  (let [uri              (get-in (read-config (io/resource "config.edn")) [:db :uri])
-        {:keys [conn db]} (mg/connect-via-uri uri)]
-    (reset! connection conn)
-    (reset! database db)))
+  (when (nil? @connection)
+    (let [uri              (get-in @config [:db :uri])
+          {:keys [conn db]} (mg/connect-via-uri uri)]
+      (reset! connection conn)
+      (reset! database db))))
+
+(defn disconnect! []
+  (when-let [conn @connection]
+    (mg/disconnect conn)
+    (reset! connection nil)
+    (reset! database nil)))
 
 (defn get-db [] @database)
