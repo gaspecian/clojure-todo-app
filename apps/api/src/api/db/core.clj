d36c183c006254f5e68bc558ee772ebbf4434e05
Gabriel Specian <ga.specian@gmail.com>
2026-05-28 22:24:38 -0300

    refactor(api): add pure db/connect and db/disconnect alongside globals

--- apps/api/src/api/db/core.clj
+++ apps/api/src/api/db/core.clj
@@ -4,6 +4,16 @@
             [aero.core :refer [read-config]]
             [clojure.java.io :as io]))
 
+(defn connect [uri]
+  (let [{:keys [conn db]} (mg/connect-via-uri uri)]
+    (mc/ensure-index db "users" (array-map :email 1) {:unique true})
+    (mc/ensure-index db "users" (array-map :login 1) {:unique true})
+    {:conn conn :db db}))
+
+(defn disconnect [conn]
+  (mg/disconnect conn))
+
+;; --- transitional global API (removed in Task 8 once all callers migrate) ---
 (defonce ^:private connection (atom nil))
 (defonce ^:private database   (atom nil))
 
@@ -12,18 +22,14 @@
 
 (defn connect! []
   (when (nil? @connection)
-    (let [uri              (get-in @config [:db :uri])
-          {:keys [conn db]} (mg/connect-via-uri uri)]
+    (let [{:keys [conn db]} (connect (get-in @config [:db :uri]))]
       (reset! connection conn)
-      (reset! database db)
-      ;; Ensure unique indexes exist
-      (mc/ensure-index db "users" (array-map :email 1) {:unique true})
-      (mc/ensure-index db "users" (array-map :login 1) {:unique true}))))
+      (reset! database db))))
+
+(defn get-db [] @database)
 
 (defn disconnect! []
   (when-let [conn @connection]
-    (mg/disconnect conn)
+    (disconnect conn)
     (reset! connection nil)
     (reset! database nil)))
-
-(defn get-db [] @database)
--- apps/api/test/api/db/core_test.clj
+++ apps/api/test/api/db/core_test.clj
@@ -6,3 +6,10 @@
   (testing "connects to MongoDB and returns a db instance"
     (db/connect!)
     (is (some? (db/get-db)))))
+
+(deftest connect-returns-conn-and-db-test
+  (testing "connect returns a map with a live conn and db"
+    (let [{:keys [conn db]} (db/connect "mongodb://localhost:27017/tododb_test")]
+      (is (some? conn))
+      (is (some? db))
+      (db/disconnect conn))))
